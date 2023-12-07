56fbd42e0f8049e7099e8e585fe3d9e91f05f9fc
alijr2018 <alijr_2018@proton.me>
2023-12-07 00:08:24 +0000

    try1

---
+++ upload.php
@@ -0,0 +1,36 @@
+<?php
+$targetDirectory = "files/"; // Directory where uploaded files will be stored
+$targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$fileType = strtolower(pathinfo($targetFile,PATHINFO_EXTENSION));
+
+
+if (file_exists($targetFile)) {
+	echo "Sorry, file already exists.";
+	$uploadOk = 0;
+}
+
+
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+	echo "Sorry, your file is too large.";
+	$uploadOk = 0;
+}
+
+
+if($fileType !== "jpg" && $fileType !== "png" && $fileType !== "jpeg"
+	&& $fileType !== "gif" ) {
+	echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+	$uploadOk = 0;
+}
+
+if ($uploadOk == 0) {
+	echo "Sorry, your file was not uploaded.";
+} else {
+	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
+		echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+	} else {
+		echo "Sorry, there was an error uploading your file, Try again in a sec.";
+	}
+}
+?>
+
