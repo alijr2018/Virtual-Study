25b4bd3f9249b71610efc3b192986fce4519f042
alijr2018 <alijr_2018@proton.me>
2023-12-11 21:04:32 +0000

    git add .=!

--- final1/login/login.php
+++ final1/login/login.php
@@ -1,15 +1,31 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	    $username = $_POST["username"];
-	        $password = $_POST["password"];
+session_start();
 
 
-	        if ($username === "demo" && $password === "password") {
-			        echo "Login successful!";
+$valid_username = 'user';
+$valid_password = 'password';
 
-				    } else {
-					            echo "Invalid username or password!";
-						        }
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	    $entered_username = $_POST['username'];
+	        $entered_password = $_POST['password'];
+
+
+	        if ($entered_username === $valid_username && $entered_password === $valid_password) {
+			        $_SESSION['username'] = $entered_username;
+
+
+				        header('Location: dashboard.php');
+				        exit();
+					    } else {
+
+						            header('Location: index.php?error=invalid_credentials');
+							            exit();
+							        }
+} else {
+
+	    header('Location: index.php');
+	        exit();
 }
 ?>
 
